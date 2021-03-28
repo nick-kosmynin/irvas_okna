1c18767d05291a1b98a2c88dcb4f08b135bb8c86
nick <kosmynin84@outlook.com>
2021-03-28 22:50:41 +0300

    модальные окна + формы

--- src/js/main.js
+++ src/js/main.js
@@ -35,25 +35,78 @@ window.addEventListener('DOMContentLoaded', () => {
     //MODAL 2
 
     const KnopkaZvonka = document.querySelector('[data-knopka]'),
-                modal2 = document.querySelector('.popup'),
-                ZakrKnopka = document.querySelector('[data-zakr]');
-    
-       KnopkaZvonka.addEventListener('click', ()=> {
-           modal2.classList.add('show');
-           modal2.classList.remove('hide');
-           
-       });         
+        modal2 = document.querySelector('.popup'),
+        ZakrKnopka = document.querySelector('[data-zakr]');
 
-       ZakrKnopka.addEventListener('click', ()=>{
+    KnopkaZvonka.addEventListener('click', () => {
+        modal2.classList.add('show');
+        modal2.classList.remove('hide');
+
+    });
+
+    ZakrKnopka.addEventListener('click', () => {
         modal2.classList.add('hide');
         modal2.classList.remove('show');
-       });
+    });
 
-       modal2.addEventListener('click', (e)=> {
-           if(e.target === modal2){
+    modal2.addEventListener('click', (e) => {
+        if (e.target === modal2) {
             modal2.classList.add('hide');
             modal2.classList.remove('show');
-           }
-       });
+        }
+    });
+
+    //FORMS
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        succes: 'Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) =>{
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+           request.setRequestHeader('content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value,key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+            
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = message.succes;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 3000);
+                }else{
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 
 });
