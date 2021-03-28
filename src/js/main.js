435d4c67ba5e20bad69c539c87824afb7f056b30
nick <kosmynin84@outlook.com>
2021-03-28 18:46:46 +0300

    создал 2 мод_окна

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,59 @@
-
-
 window.addEventListener('DOMContentLoaded', () => {
 
     // MODAL
 
-const modalTrigger = document.querySelector('[data-modal]'),
-modal = document.querySelector('.popup_engineer'),
-modalCloseBtn = document.querySelector('[data-close]');
+    const modalTrigger = document.querySelector('[data-modal]'),
+        modal = document.querySelector('.popup_engineer'),
+        modalCloseBtn = document.querySelector('[data-close]');
 
-modalTrigger.addEventListener('click', ()=>{
-modal.classList.add('show');
-modal.classList.remove('hide');
-document.body.style.overflow = 'hidden';
-});  
+    modalTrigger.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    });
 
-function closeModal(){
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    document.body.style.overflow = '';
-}
-
-modalCloseBtn.addEventListener('click', closeModal ); 
-
-modal.addEventListener('click', (e)=>{
-     if(e.target === modal){
-        closeModal();
-     }
-});
-
-document.addEventListener('keydown', (e) => {
-    if(e.code === "Escape"){
-        closeModal();
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
     }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape") {
+            closeModal();
+        }
+    });
+
+    //MODAL 2
+
+    const KnopkaZvonka = document.querySelector('[data-knopka]'),
+                modal2 = document.querySelector('.popup'),
+                ZakrKnopka = document.querySelector('[data-zakr]');
+    
+       KnopkaZvonka.addEventListener('click', ()=> {
+           modal2.classList.add('show');
+           modal2.classList.remove('hide');
+           
+       });         
+
+       ZakrKnopka.addEventListener('click', ()=>{
+        modal2.classList.add('hide');
+        modal2.classList.remove('show');
+       });
+
+       modal2.addEventListener('click', (e)=> {
+           if(e.target === modal2){
+            modal2.classList.add('hide');
+            modal2.classList.remove('show');
+           }
+       });
+
 });
-
-
-
-});
-      
